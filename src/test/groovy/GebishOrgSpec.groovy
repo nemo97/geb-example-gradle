94e58a8ce5189673b3d50c46102ef8342eb7c842
Subhas <subhas.sing@gmail.com>
2018-04-12 22:09:28 -0700

    move pages to page folder

--- src/test/groovy/GebishOrgSpec.groovy
+++ src/test/groovy/GebishOrgSpec.groovy
@@ -1,21 +1,23 @@
 import geb.spock.GebSpec
+import pages.GebishOrgHomePage
+import pages.TheBookOfGebPage
 
 class GebishOrgSpec extends GebSpec {
 
     def "can get to the current Book of Geb"() {
         when:
-        to GebishOrgHomePage
+            to GebishOrgHomePage
 
         and:
-        manualsMenu.open()
+            manualsMenu.open()
 
         then:
-        manualsMenu.links[0].text().startsWith("current")
+            manualsMenu.links[0].text().startsWith("current")
 
         when:
-        manualsMenu.links[0].click()
+            manualsMenu.links[0].click()
 
         then:
-        at TheBookOfGebPage
+            at TheBookOfGebPage
     }
 }
